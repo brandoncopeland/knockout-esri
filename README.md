2e5fa4c1dc8f10277a19687bc37f6e3f37ea95b2
brandoncopeland <bcopeland@gmail.com>
2012-08-09 17:36:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Knockout ESRI Bindings
+
+Custom [knockout.js](http://knockoutjs.com/) bindings for [ESRI Javascript API](http://help.arcgis.com/en/webapi/javascript/arcgis) widgets (currently just the map widget).
+
+## Usage
+
+	<div id="map" data-bind="esriMap: { mapObject: mapWidget, isLoaded: isMapLoaded, extent: currentMapExtent, layers: mapLayers, extentRequest: mapExtentRequest, levelRequest: mapLevelRequest, onClick: doSomething }"></div>
+	 
+	<script type="text/javascript">
+	    var viewModel = {
+	    	mapWidget: ko.observable(),
+	    	isMapLoaded: ko.observable(),
+	    	currentMapExtent: ko.observable(),
+	    	mapLayers: ko.observableArray([]),
+	    	mapExtentRequest: ko.observable(),
+	    	mapLevelRequest: ko.observable(),
+	    	doSomething: function () {
+	    		alert('i did it');
+	    	}
+	    };
+	</script>
+
+## Parameters
+
+Parameters are all optional.
+
+__mapObject__: esri.Map, ESRI map widget
+
+__isLoaded__: boolean, true if map has successfully loaded with at least 1 layer
+
+__extent__: esri.geometry.Extent, current map extent (intended for reading only)
+
+__layers__: array of esri.layers.Layer, current map layers, push to array to add layers to the map
+
+__extentRequest__: set to esri.geometry.Extent for map extent change, value will immediately reset to `undefined` after
+
+__levelRequest__: set to `{ point:[esri.geometry.Point], level:[number] }` object for map center/level change, point and level describe new map center and tile level, either or both properties may be set, value will immediately reset to `undefined` after
+
+__onClick__: command to fire on map click, args are identical to esri.map onClick event
+
+## Requirements
+
+[jQuery](http://jquery.com/)
+
+[knockout.js](http://knockoutjs.com/)
+
+[ESRI Javascript API](http://help.arcgis.com/en/webapi/javascript/arcgis)
---
+++ build/knockout-esri.js
@@ -0,0 +1,143 @@
+//knockout-esri v0.1.0 | (c) 2012 BrandonCopeland
+
+(function (window, $, ko, dojo, esri) {
+	"use strict";
+
+    ko.esri = ko.esri || {};
+
+    ko.esri.MapBinding = function () {
+		var self = this;
+
+		// name of accessors to use within binding...
+		// <div id="map" data-bind="esriMap: { mapObject: ..., layers: ..., extent: ..., etc... }"></div>
+		var mapObjectAccessor = 'mapObject';			// esri.Map object created
+		var mapLoadedAccessor = 'isLoaded';				// boolean, true if map is loaded with at least 1 layer
+		var extentAccessor = 'extent';					// current esri.geometry.Extent of the map, only set internally
+		var layersAccessor = 'layers';					// array of esri.layers.Layer, current map layers, push to to add layer to map
+		var extentRequestAccessor = 'extentRequest';	// set to esri.geometry.Extent to perform map extent change, resets after
+		var levelRequestAccessor = 'levelRequest';		// { esri.geometry.Point, tile level }, may set either or both to center at and zoom
+		var onClickAccessor = 'onClick';				// command to perform on map click, parameters are typical map click args
+
+		// map: esri.Map
+		// layerArray: array of esri.layers.Layer
+		var updateMapLayers = function (map, layerArray) {
+			if (map && layerArray) {
+				$.each(layerArray, function (index, layer) {
+					if ($.inArray(layer.id, map.layerIds) === -1 && $.inArray(layer.id, map.graphicsLayerIds) === -1) {
+						map.addLayer(layer, index);
+					}
+
+					// TODO: Handle removing, reordering
+				});
+			}
+		};
+
+		// add handlers to update map on browser resize
+		// map: esri.Map
+		// TODO: maybe this should be on map container resize instead?
+		// TODO: does dojo.disconnect need to be called? does it matter?
+		var handleMapReposition = function (map) {
+			var timer;
+			dojo.connect(map, 'onLoad', function (evt) {
+				dojo.connect(window, 'onresize', function (evt) {
+					if ($(map.container).is(':visible')) {
+						window.clearTimeout(timer);
+						timer = window.setTimeout(function () {
+							map.resize();
+							map.reposition();
+						}, 100);
+					}
+				});
+			});
+		};
+
+		var binding = {
+			init: function (element, valueAccessor) {
+				dojo.require('esri.map');
+
+				var mapElementId = element.id;
+				var va = valueAccessor();
+
+				var mapOptions = {
+					fadeOnZoom: true,
+					fitExtent: true,
+					logo: false
+				};
+
+				dojo.ready(function () {
+
+					if (va[extentRequestAccessor] && va[extentRequestAccessor]()) {
+						mapOptions = $.extend(mapOptions, {
+							extent: va[extentRequestAccessor]()
+						});
+						va[extentRequestAccessor](undefined); // reset
+					}
+
+					var map = new esri.Map(mapElementId, mapOptions);
+
+					if (va[mapObjectAccessor]) {
+						va[mapObjectAccessor](map);
+					}
+
+					handleMapReposition(map);
+
+					if (va[mapLoadedAccessor]) {
+						dojo.connect(map, 'onLoad', function (evt) {
+							va[mapLoadedAccessor](map.loaded);
+						});
+					}
+
+					if (va[onClickAccessor]) {
+						dojo.connect(map, 'onClick', va[onClickAccessor]);
+					}
+
+					if (va[extentAccessor]) {
+						dojo.connect(map, 'onExtentChange', function (newExtent) {
+							va[extentAccessor](newExtent);
+						});
+					}
+
+					if (va[extentRequestAccessor]) {
+						ko.computed(function () {
+							if (va[extentRequestAccessor]()) { // avoid reset setting
+								map.setExtent(va[extentRequestAccessor](), mapOptions.fitExtent);
+								va[extentRequestAccessor](undefined); // reset
+							}
+						});
+					}
+
+					if (va[levelRequestAccessor]) {
+						ko.computed(function () {
+							if (va[levelRequestAccessor]()) {
+								var req = va[levelRequestAccessor]();
+
+								// support setting point, level, or both
+								if (req.point && req.level) {
+									map.centerAndZoom(req.point, req.level);
+								} else if (req.point) {
+									map.centerAt(req.point);
+								} else if (req.level) {
+									map.setLevel(req.level);
+								}
+
+								va[levelRequestAccessor](undefined); // reset
+							}
+						});
+					}
+
+					if (va[layersAccessor]) {
+						ko.computed(function () {
+							updateMapLayers(map, va[layersAccessor]());
+						});
+					}
+
+				});
+			}
+		};
+
+		ko.bindingHandlers.esriMap = binding;
+	};
+
+	ko.esri.MapBinding = new ko.esri.MapBinding();
+
+}(window, jQuery, ko, dojo, esri));
