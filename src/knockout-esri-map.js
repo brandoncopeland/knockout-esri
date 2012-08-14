1801e1e0a7a5f975c8b15e377be9708880cc330c
brandoncopeland <bcopeland@gmail.com>
2012-08-14 15:38:34 -0500

    removed extentRequest and levelRequest
    extentRequest and levelRequest only clutter this binding. They are not
    commands directly and features can be be accomplished by setting extent
    or level on the map widget in the vm directly. removed.

--- src/knockout-esri-map.js
+++ src/knockout-esri-map.js
@@ -8,13 +8,11 @@
 
 		// name of accessors to use within binding...
 		// <div id="map" data-bind="esriMap: { mapObject: ..., layers: ..., extent: ..., etc... }"></div>
-		var mapObjectAccessor = 'mapObject';			// esri.Map object created
-		var mapLoadedAccessor = 'isLoaded';				// boolean, true if map is loaded with at least 1 layer
-		var extentAccessor = 'extent';					// current esri.geometry.Extent of the map, only set internally
-		var layersAccessor = 'layers';					// array of esri.layers.Layer, current map layers, push to to add layer to map
-		var extentRequestAccessor = 'extentRequest';	// set to esri.geometry.Extent to perform map extent change, resets after
-		var levelRequestAccessor = 'levelRequest';		// { esri.geometry.Point, tile level }, may set either or both to center at and zoom
-		var onClickAccessor = 'onClick';				// command to perform on map click, parameters are typical map click args
+		var mapObjectAccessor = 'mapObject';	// esri.Map object created
+		var mapLoadedAccessor = 'isLoaded';		// boolean, true if map is loaded with at least 1 layer
+		var extentAccessor = 'extent';			// current esri.geometry.Extent of the map, only set internally
+		var layersAccessor = 'layers';			// array of esri.layers.Layer, current map layers, push to to add layer to map
+		var onClickAccessor = 'onClick';		// command to perform on map click, parameters are typical map click args
 
 		// map: esri.Map
 		// layerArray: array of esri.layers.Layer
@@ -64,13 +62,6 @@
 
 				dojo.ready(function () {
 
-					if (va[extentRequestAccessor] && va[extentRequestAccessor]()) {
-						mapOptions = $.extend(mapOptions, {
-							extent: va[extentRequestAccessor]()
-						});
-						va[extentRequestAccessor](undefined); // reset
-					}
-
 					var map = new esri.Map(mapElementId, mapOptions);
 
 					if (va[mapObjectAccessor]) {
@@ -92,34 +83,6 @@
 					if (va[extentAccessor]) {
 						dojo.connect(map, 'onExtentChange', function (newExtent) {
 							va[extentAccessor](newExtent);
-						});
-					}
-
-					if (va[extentRequestAccessor]) {
-						ko.computed(function () {
-							if (va[extentRequestAccessor]()) { // avoid reset setting
-								map.setExtent(va[extentRequestAccessor](), mapOptions.fitExtent);
-								va[extentRequestAccessor](undefined); // reset
-							}
-						});
-					}
-
-					if (va[levelRequestAccessor]) {
-						ko.computed(function () {
-							if (va[levelRequestAccessor]()) {
-								var req = va[levelRequestAccessor]();
-
-								// support setting point, level, or both
-								if (req.point && req.level) {
-									map.centerAndZoom(req.point, req.level);
-								} else if (req.point) {
-									map.centerAt(req.point);
-								} else if (req.level) {
-									map.setLevel(req.level);
-								}
-
-								va[levelRequestAccessor](undefined); // reset
-							}
 						});
 					}
 
